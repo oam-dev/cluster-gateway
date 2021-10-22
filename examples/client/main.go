58122e25f98ed7c28571eae020f3955eae324dd9
yue9944882 <291271447@qq.com>
2021-10-22 14:59:52 +0800

    adding multi-cluster roundtripper wrapper

--- examples/client/main.go
+++ examples/client/main.go
@@ -3,58 +3,58 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/oam-dev/cluster-gateway/pkg/apis/cluster/v1alpha1"
-	"github.com/oam-dev/cluster-gateway/pkg/generated/clientset/versioned"
+	multicluster "github.com/oam-dev/cluster-gateway/pkg/apis/cluster/transport"
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var kubeconfig string
+var clusterName string
 
 func main() {
+
 	cmd := cobra.Command{
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 			if err != nil {
 				return err
 			}
-			gatewayClient := versioned.NewForConfigOrDie(cfg)
-			_, err = gatewayClient.ClusterV1alpha1().ClusterGateways().Create(
-				context.TODO(),
-				&v1alpha1.ClusterGateway{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-					Spec: v1alpha1.ClusterGatewaySpec{
-						Provider: "foo",
-						Access: v1alpha1.ClusterAccess{
-							Endpoint: cfg.Host,
-							CABundle: cfg.CAData,
-							Insecure: pointer.BoolPtr(true),
-							Credential: &v1alpha1.ClusterAccessCredential{
-								Type:                v1alpha1.CredentialTypeServiceAccountToken,
-								ServiceAccountToken: "abc",
-							},
-						},
-					},
-				},
-				metav1.CreateOptions{})
+			cfg.Wrap(multicluster.NewClusterGatewayRoundTripper)
+
+			// Native kubernetes client example
+			nativeClient := kubernetes.NewForConfigOrDie(cfg)
+			defaultNs, err := nativeClient.CoreV1().Namespaces().Get(
+				multicluster.WithMultiClusterContext(context.TODO(), clusterName),
+				"default",
+				metav1.GetOptions{})
+			fmt.Printf("Native client get default namespace: %v\n", defaultNs)
+
+			// Controller-runtime client example
+			controllerRuntimeClient, err := client.New(cfg, client.Options{})
 			if err != nil {
-				return err
+				panic(err)
 			}
-			testClusterClient := gatewayClient.ClusterV1alpha1().ClusterGateways().RESTClient("test")
-			resp, err := testClusterClient.Get().AbsPath("/healthz").DoRaw(context.TODO())
+			ns := &corev1.Namespace{}
+			err = controllerRuntimeClient.Get(
+				multicluster.WithMultiClusterContext(context.TODO(), clusterName),
+				types.NamespacedName{Name: "default"},
+				ns)
 			if err != nil {
-				return err
+				panic(err)
 			}
-			fmt.Printf("Proxied Response: %v\n", string(resp))
+			fmt.Printf("Controller-runtime client get default namespace: %v\n", ns)
 			return nil
 		},
 	}
 
 	cmd.Flags().StringVarP(&kubeconfig, "kubeconfig", "", "", "the client kubeconfig")
+	cmd.Flags().StringVarP(&clusterName, "cluster-name", "", "", "the target cluster name")
+
 	if err := cmd.Execute(); err != nil {
 		panic(err)
 	}
