ef7e418a50b653c0e30ec34ed40bed252cba871e
yue9944882 <291271447@qq.com>
2022-02-10 16:37:59 +0800

    test healthiness after "health" subresource update
    Signed-off-by: yue9944882 <291271447@qq.com>

--- e2e/roundtrip/clustergateway.go
+++ e2e/roundtrip/clustergateway.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	"github.com/oam-dev/cluster-gateway/e2e/framework"
+	multicluster "github.com/oam-dev/cluster-gateway/pkg/apis/cluster/transport"
 	clusterv1alpha1 "github.com/oam-dev/cluster-gateway/pkg/apis/cluster/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
 
 	. "github.com/onsi/ginkgo"
@@ -101,6 +103,35 @@ var _ = Describe("Basic RoundTrip Test", func() {
 			Expect(updated).NotTo(BeNil())
 			Expect(updated.Status.Healthy).To(BeTrue())
 			Expect(updated.Status.HealthyReason).To(Equal(clusterv1alpha1.HealthyReasonTypeConnectionTimeout))
+		})
+
+	It("Probing cluster health (raw)",
+		func() {
+			resp, err := f.HubNativeClient().Discovery().
+				RESTClient().
+				Get().
+				AbsPath(
+					"apis/cluster.core.oam.dev/v1alpha1/clustergateways",
+					f.TestClusterName(),
+					"proxy",
+					"healthz",
+				).DoRaw(context.TODO())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(resp)).To(Equal("ok"))
+		})
+
+	It("Probing cluster health (context)",
+		func() {
+			cfg := f.HubRESTConfig()
+			cfg.WrapTransport = multicluster.NewClusterGatewayRoundTripper
+			multiClusterClient, err := kubernetes.NewForConfig(cfg)
+			Expect(err).NotTo(HaveOccurred())
+			resp, err := multiClusterClient.RESTClient().
+				Get().
+				AbsPath("healthz").
+				DoRaw(multicluster.WithMultiClusterContext(context.TODO(), f.TestClusterName()))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(resp)).To(Equal("ok"))
 		})
 
 })
