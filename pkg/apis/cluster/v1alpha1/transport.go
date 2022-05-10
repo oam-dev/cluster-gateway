c51e96582b8b9b70e9875f6068984e640122f244
yue9944882 <291271447@qq.com>
2022-05-10 15:28:10 +0800

    basing onto ANP upstream's connection leakage fix
    Signed-off-by: yue9944882 <291271447@qq.com>

--- pkg/apis/cluster/v1alpha1/transport.go
+++ pkg/apis/cluster/v1alpha1/transport.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 	"time"
 
-	konnectivity "github.com/oam-dev/cluster-gateway/forked/konnectivity-client/pkg/client"
 	"github.com/oam-dev/cluster-gateway/pkg/config"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
@@ -15,6 +14,7 @@ import (
 	"google.golang.org/grpc/keepalive"
 	k8snet "k8s.io/apimachinery/pkg/util/net"
 	restclient "k8s.io/client-go/rest"
+	konnectivity "sigs.k8s.io/apiserver-network-proxy/konnectivity-client/pkg/client"
 	"sigs.k8s.io/apiserver-network-proxy/pkg/util"
 )
 
